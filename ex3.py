b8fd8c96df61c8028d00a2ae69d5890324f7e02d
ashermanwmf <asherman@wikimedia.org>
2015-09-21 17:12:50 -0400

    first commit

---
+++ ex3.py
@@ -0,0 +1,36 @@
+# This prints out the message "I will now count my chikens:"
+print "I will now count my chikens:"
+
+# This prints out "Hens" followed by 30 divided by 6 plus 25
+# The next line prints the "Rooster" followed by the remander of
+print "Hens", 25 + 30 / 6
+print "Rooster", 100 - 25 * .75
+
+# Print out "Now I will count the eggs:"
+print "Now I will count the eggs:"
+
+# PEMDAS ordered math that is printed
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# Prints out "Is it true that 3 + 2 < 5 - 7?"
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# PEMDAS ordered math that is printed
+print 3 + 2 < 5 - 7
+
+# Prints out question and answer with math
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+# Answers why the above question was false
+print "Oh, that's why it's False."
+
+# Prints out statement
+print "How about some more."
+
+#Prints out answers to questions with true false statements 
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+
+# I CANT DO THIS Notice the math seems "wrong"? There are no fractions, only whole numbers. You need to use a "floating point" number, which is a number with a decimal point, as in 10.5, or 0.89, or even 3.0. Too much math :(#

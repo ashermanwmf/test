bf601362c3e5af3da4387ba1a2b2db68ea002e62
ashermanwmf <asherman@wikimedia.org>
2015-09-30 23:03:23 -0400

    finished lesson 40

--- ex40.py
+++ ex40.py
@@ -1,28 +1,27 @@
 import mystuff
 
-
 class Song(object):
-    
+
     def __init__(self, lyrics):
         self.lyrics = lyrics
-        
+
     def sing_me_a_song(self):
         for line in self.lyrics:
-            print line 
+            print line
 
-# I want this to do the same thing as bulls_on_parade.sing_me_a_song()
-def hbday(i):
-    i = ["Happy birthday to you", 
-            "I dont want to get sued",
-            "So I'll stop right there"]
-    return i
+#This is what I was trying to do
+happy_bday = ["Happy birthday to you",
+                   "I don't want to get sued",
+                   "So I'll stop right there"]
+
+#this is what I was trying to do
+hpbday = Song(happy_bday)
 
 bulls_on_parade = Song(["They rally around tha family",
                         "With pockets full of shells"])
 
-
-Song.sing_me_a_song(hbday)
-
+#this is what I was trying to do
+hpbday.sing_me_a_song()
 
 bulls_on_parade.sing_me_a_song()
 
